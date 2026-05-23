0458b6b6bc53856d37ba7f99009a0648fc49b46d
lizaveta-06 <m.e.a06@mail.ru>
2026-05-23 19:57:27 +0700

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import './Login.css';
+
+import waveImg from '../../Vector.png'; 
+import circlesImg from '../../elips.png'; 
+
+function Login() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="login-container">
+      //эллипс
+      <div className="circles-image-wrapper">
+        <img src={circlesImg} alt="circles decoration" className="circles-bg" />
+      </div>
+      //волна
+      <div className="wave-wrapper">
+        <img src={waveImg} alt="wave decoration" className="wave-bg" />
+      </div>
+      //Авторизация
+      <div className="auth-form">
+        <h2 className="auth-title">АВТОРИЗАЦИЯ</h2>
+        //поле логин
+        <div className="input-field">
+          <div className="icon-box">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1.5">
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+              <circle cx="12" cy="7" r="4" />
+            </svg>
+          </div>
+          <input 
+            type="text" 
+            placeholder="ЛОГИН" 
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </div>
+        //поле пароль
+        <div className="input-field">
+          <div className="icon-box">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1.5">
+              <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
+              <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+            </svg>
+          </div>
+          <input 
+            type="password" 
+            placeholder="ПАРОЛЬ" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        //кнопка войти
+        <button className="submit-btn">ВОЙТИ</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Login;
